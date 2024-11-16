59d3f0aa55f4276d0937c0ebd6e0f9ce80c9f84a
Alan Vargas <137660545+Alang315@users.noreply.github.com>
2024-11-16 08:04:29 -0600

    Feature spa schedule (#43)
    * ScheduleWithoutSexField
    
    * SpaSchedule working but now takes the sex from each appointment

--- app/src/main/java/com/example/spaTi/ui/appointments/AppointmentViewModel.kt
+++ app/src/main/java/com/example/spaTi/ui/appointments/AppointmentViewModel.kt
@@ -31,6 +31,10 @@ class AppointmentViewModel @Inject constructor (
     val getAppointmentsByDate: LiveData<UiState<List<Appointment>>>
         get() = _getAppointmentsByDate
 
+    private val _getAppointmentsByDateOnSpaSchedule = MutableLiveData<UiState<List<Map<String, Any>>>>()
+    val getAppointmentsByDateOnSpaSchedule: LiveData<UiState<List<Map<String, Any>>>>
+        get() = _getAppointmentsByDateOnSpaSchedule
+
     private val _getAppointmentByMonth = MutableLiveData<UiState<Map<LocalDate, List<Appointment>>>>()
     val getAppointmentByMonth: LiveData<UiState<Map<LocalDate, List<Appointment>>>>
         get() = _getAppointmentByMonth
@@ -68,9 +72,19 @@ class AppointmentViewModel @Inject constructor (
         repository.getAppointmentsByDate(spaId, date) { _getAppointmentsByDate.value = it }
     }
 
+    fun getAppointmentsByDateOnAppointmentsSchedule(spaId: String, date: LocalDate) {
+        _getAppointmentsByDateOnSpaSchedule.value = UiState.Loading
+        repository.getAppointmentsByDateOnAppointmentsSchedule(spaId, date) { _getAppointmentsByDateOnSpaSchedule.value = it }
+    }
+
     fun getAppointmentByMonth(spaId: String, yearMonth: YearMonth) {
         _getAppointmentByMonth.value = UiState.Loading
         repository.getAppointmentByMonth(spaId, yearMonth) { _getAppointmentByMonth.value = it }
+    }
+
+    fun getAppointmentByMonthOnAppointmentsSchedule(spaId: String, yearMonth: YearMonth) {
+        _getAppointmentByMonth.value = UiState.Loading
+        repository.getAppointmentByMonthOnAppointmentsSchedule(spaId, yearMonth) { _getAppointmentByMonth.value = it }
     }
 
     fun setAppointmentAccepted(AppointmentId: String) {
--- app/src/main/java/com/example/spaTi/ui/spaHome/SpaHomeFragment.kt
+++ app/src/main/java/com/example/spaTi/ui/spaHome/SpaHomeFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.spaTi.R
+import com.example.spaTi.data.models.Spa
 import com.example.spaTi.databinding.FragmentSpaHomeBinding
 import com.example.spaTi.ui.appointments.AppointmentViewModel
 import com.example.spaTi.ui.auth.AuthViewModel
@@ -18,12 +19,15 @@ import com.example.spaTi.util.hide
 import com.example.spaTi.util.show
 import com.example.spaTi.util.toast
 import dagger.hilt.android.AndroidEntryPoint
+import java.sql.Types.NULL
 
 @AndroidEntryPoint
 class SpaHomeFragment : Fragment() {
     val TAG: String = "SpaHomeFragment"
     val authViewModel: SpaAuthViewModel by viewModels()
     val viewModel: AppointmentViewModel by viewModels()
+    var objspa: Spa? = null
+    var state = null
 
     private var _binding: FragmentSpaHomeBinding? = null
     private val binding get() = _binding!!
@@ -41,19 +45,31 @@ class SpaHomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         appointmentsObserver()
 
-        binding.services.setOnClickListener {
-            findNavController().navigate(R.id.action_spaHomeFragment_to_servicesFragment)
-        }
-        binding.logout.setOnClickListener {
-            authViewModel.logout {
-                findNavController().navigate(R.id.action_spaHomeFragment_to_loginFragment)
+        authViewModel.getSession {
+
+            objspa = it
+
+            binding.services.setOnClickListener {
+                findNavController().navigate(R.id.action_spaHomeFragment_to_servicesFragment, Bundle().apply { putParcelable("spa", objspa) })
             }
-        }
-        binding.configuration.setOnClickListener {
-            findNavController().navigate(R.id.action_spaHomeFragment_to_myaccountspaFragment)
-        }
-        binding.solicitud.setOnClickListener {
-            findNavController().navigate(R.id.action_spaHomeFragment_to_appointmentlisitngFragment)
+
+            binding.logout.setOnClickListener {
+                authViewModel.logout {
+                    findNavController().navigate(R.id.action_spaHomeFragment_to_loginFragment, Bundle().apply { putParcelable("spa", objspa) })
+                }
+            }
+
+            binding.configuration.setOnClickListener {
+                findNavController().navigate(R.id.action_spaHomeFragment_to_myaccountspaFragment, Bundle().apply { putParcelable("spa", objspa) })
+            }
+
+            binding.solicitud.setOnClickListener {
+                findNavController().navigate(R.id.action_spaHomeFragment_to_appointmentlisitngFragment, Bundle().apply { putParcelable("spa", objspa) })
+            }
+
+            binding.agenda.setOnClickListener {
+                findNavController().navigate(R.id.action_spaHomeFragment_to_spaScheduleFragment, Bundle().apply { putParcelable("spa", objspa) })
+            }
         }
     }
 
